e77c40e2a1a297e45540b36f394198578b07f781
Emily81926 <emily81926@gmail.com>
2023-01-28 23:31:23 +0800

    revise: revise free cash flow reducer to get only five data

--- src/reducers/stockReducers/freeCashReducer.js
+++ src/reducers/stockReducers/freeCashReducer.js
@@ -1,16 +1,20 @@
 import { FETCH_FREE_CASH_FLOW } from '../../actions/types'
 
+function getFreeCashFLow(array){
+  for(let i = 0; i < 5; i++){
+    let Obj = {}
+    Obj["date"] = array[i].date;
+    Obj["symbol"] = array[i].symbol;
+    Obj["freeCashFlow"] = array[i].freeCashFlow;
+    return Obj 
+  }
+}
+
 // eslint-disable-next-line 
-export default (state = [], action) => {
+export default async (state = [], action) => {
   switch (action.type) {
      case FETCH_FREE_CASH_FLOW:
-      const CashFlow = action.payload.map(cash => {
-        let newObj = {};
-        newObj["date"] = cash.date;
-        newObj["symbol"] = cash.symbol;
-        newObj["freeCashFlow"] = cash.freeCashFlow;
-        return newObj
-      });
+      const CashFlow = await getFreeCashFLow(action.payload);
       return CashFlow;
     default:
       return state;
